92c8a3042d157e3eb2c318e99dc0b86be1c82dcc
Face <69168154+face-hh@users.noreply.github.com>
2022-01-18 17:18:20 +0200

    [TEST] Testing member option in profile.js [3]

--- src/Commands/RPG/profile.js
+++ src/Commands/RPG/profile.js
@@ -7,7 +7,7 @@ module.exports = class PingInteraction extends InteractionBase {
 			description: 'See your profile!',
 			opions: [{
 				name: 'user',
-				type: 6,
+				type: 5,
 				description: 'You want to see a profile of a user in particular?',
 			}],
 			cooldown: 6000,
