33c4823488b9c86541cfa7fe074d116a1903f994
Face <69168154+face-hh@users.noreply.github.com>
2022-01-18 17:34:34 +0200

    Update profile.js

--- src/Commands/RPG/profile.js
+++ src/Commands/RPG/profile.js
@@ -1,5 +1,6 @@
 const InteractionBase = require('../../Structures/CommandBase');
-
+const { Constants } = require('eris');
+console.log(Constants.CommandOptionTypes)
 module.exports = class PingInteraction extends InteractionBase {
 	constructor(...args) {
 		super(...args, {
@@ -7,7 +8,7 @@ module.exports = class PingInteraction extends InteractionBase {
 			description: 'See your profile!',
 			opions: [{
 				name: 'user',
-				type: 4,
+				type: Constants.CommandOptionTypes.MEMBER,
 				description: 'You want to see a profile of a user in particular?',
 			}],
 			cooldown: 6000,
