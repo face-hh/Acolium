df50d0a2abe85cd1d10e246743fa06d4a883902d
Face <xxfacexc@gmail.com>
2022-01-22 18:15:50 +0200

    [FIX] Misspelling

--- src/Events/guildDelete.js
+++ src/Events/guildDelete.js
@@ -7,6 +7,6 @@ module.exports = class extends Event {
 		});
 	}
 	async run(guild) {
-		this.client.getRESTChannels('934187656873644042').createMessage(`Left **${guild.name}** with **${guild.memberCount}** members!`);
+		this.client.getRESTChannel('934187656873644042').createMessage(`Left **${guild.name}** with **${guild.memberCount}** members!`);
 	}
 };
