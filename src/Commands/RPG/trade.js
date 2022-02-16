b88a1da229a030996b4a078112bfaa8acac5dea8
Face <69168154+face-hh@users.noreply.github.com>
2022-02-16 16:38:28 +0200

    Update trade.js

--- src/Commands/RPG/trade.js
+++ src/Commands/RPG/trade.js
@@ -164,11 +164,11 @@ module.exports = class PingInteraction extends InteractionBase {
 		if (where2 < itemUser.amount) return interaction.createFollowup(`${user.username} doesn't have that many items!`);
 
 		await confirmation(async () => {
-			console.log(data.Backpack[whereIsTheItem[0]][specifiedItemData.name.replace(/ /gi, '')])
-			console.log(data2.Backpack[whereIsTheItem2[0]][specifiedItemData2.name.replace(/ /gi, '')])
-			console.log(itemAuthor.item, itemUser.item)
 			itemAuthor.item === 'Coins' ? data.Coins -= itemAuthor.amount : data.Backpack[whereIsTheItem[0]][specifiedItemData.name.replace(/ /gi, '')] -= itemAuthor.amount;
-			itemUser.item === 'Coins' ? data2.Coins += itemUser.amount : data2.Backpack[whereIsTheItem2[0]][specifiedItemData2.name.replace(/ /gi, '')] += itemUser.amount;
+			itemUser.item === 'Coins' ? data2.Coins -= itemUser.amount : data2.Backpack[whereIsTheItem2[0]][specifiedItemData2.name.replace(/ /gi, '')] -= itemUser.amount;
+
+			itemAuthor.item === 'Coins' ? data.Coins += itemUser.amount : data.Backpack[whereIsTheItem[0]][specifiedItemData.name.replace(/ /gi, '')] += itemUser.amount;
+			itemUser.item === 'Coins' ? data2.Coins -= itemAuthor.amount : data2.Backpack[whereIsTheItem2[0]][specifiedItemData2.name.replace(/ /gi, '')] += itemAuthor.amount;
 
 			await this.client.db.forceUpdate({ UserId: interaction.member.id }, data, require('../../Schemas/Users'));
 			await this.client.db.forceUpdate({ UserId: user.id }, data2, require('../../Schemas/Users'));
