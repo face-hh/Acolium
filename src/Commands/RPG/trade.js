e12d86a1512fcbee6520d008aaf3cba3a02ae82b
Face <69168154+face-hh@users.noreply.github.com>
2022-02-16 16:21:00 +0200

    Update trade.js

--- src/Commands/RPG/trade.js
+++ src/Commands/RPG/trade.js
@@ -162,7 +162,7 @@ module.exports = class PingInteraction extends InteractionBase {
 		if (where < itemAuthor.amount) return interaction.createFollowup('You don\'t have that many items!');
 		if (where2 < itemUser.amount) return interaction.createFollowup(`${user.username} doesn't have that many items!`);
 
-		await confirmation(() => {
+		await confirmation(async () => {
 			itemAuthor.item === 'Coins' ? data.Coins -= itemAuthor.amount : data.Backpack[whereIsTheItem[0]][databaseItemName] -= itemAuthor.amount;
 			itemUser.item === 'Coins' ? data2.Coins += itemUser.amount : data2.Backpack[whereIsTheItem2[0]][databaseItemName2] += itemUser.amount;
 
