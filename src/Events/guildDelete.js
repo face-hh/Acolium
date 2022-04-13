9355fab2ff0072fd21076eaa77a3f37036bf8fa1
Face <69168154+face-hh@users.noreply.github.com>
2022-04-13 16:43:02 +0300

    Update guildDelete.js

--- src/Events/guildDelete.js
+++ src/Events/guildDelete.js
@@ -2,9 +2,7 @@ const Event = require('../Structures/EventBase');
 
 module.exports = class extends Event {
 	constructor(...args) {
-		super(...args, {
-			once: true,
-		});
+		super(...args, {});
 	}
 	async run(guild) {
 		this.client.createMessage('934187656873644042', `Left **${guild.name}** with **${guild.memberCount}** members!`);
